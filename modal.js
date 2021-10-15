fd64de74278837fd817dbcef9a3f7f1f346e8bcd
Jean-Christophe Urbain <jcurbain.admin@gmail.com>
2021-10-15 16:39:22 +0200

    Fermeture modale + validation Prénom + nom + mail

--- modal.js
+++ modal.js
@@ -20,4 +20,94 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
+const close = document.querySelector(".close");
+close.addEventListener("click", closeModal);
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Implémenter entrées formulaire
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birthDate = document.getElementById("birthDate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location1");
+const location4 = document.getElementById("location1");
+const location5 = document.getElementById("location1");
+const location6 = document.getElementById("location1");
+
+// Erreurs
+const errorFirst = document.getElementById("error-first");
+const errorLast = document.getElementById("error-last");
+const errorMail = document.getElementById("error-mail");
+
+// Empecher de soumettre le formulaire
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Fonction validation des champs du formulaire
+function validate() {
+  let firstValid;
+  let regexName = /^[a-z ,.'-]+$/i;
+
+  if (
+    !firstName.value.match(regexName) ||
+    firstName.value == " " ||
+    firstName.value == null ||
+    firstName.value.length < 2
+  ) {
+    errorFirst.innerText =
+      "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
+    errorFirst.style.color = "red";
+    errorFirst.style.fontSize = "0.8rem";
+    errorFirst.style.marginTop = "10px";
+    firstName.style.border = "solid red 2px";
+  } else {
+    errorFirst.style.display = "none";
+    firstName.style.border = "none";
+    firstValid = true;
+  }
+
+  let lastValid;
+
+  if (
+    !lastName.value.match(regexName) ||
+    lastName.value == " " ||
+    lastName.value == null ||
+    lastName.value.length < 2
+  ) {
+    errorLast.innerText =
+      "Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
+    errorLast.style.color = "red";
+    errorLast.style.fontSize = "0.8rem";
+    errorLast.style.marginTop = "10px";
+    lastName.style.border = "solid red 2px";
+  } else {
+    errorLast.style.display = "none";
+    lastName.style.border = "none";
+    lastValid = true;
+  }
+
+  let eMailValid;
+  let regexMail =
+    /^[a-z0-9!#$%&'*+/=?^_`{~-]+((\.[a-z0-9!#$%&'*+/=?^_`{~-]+)?)+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?$/; //RFC 2822
+
+  if (!eMail.value.match(regexMail)) {
+    errorMail.innerText = "Veuillez renseigner une adresse mail valide";
+    errorMail.style.color = "red";
+    errorMail.style.fontSize = "0.8rem";
+    errorMail.style.marginTop = "10px";
+    eMail.style.border = "solid red 2px";
+  } else {
+    errorMail.style.display = "none";
+    eMail.style.border = "none";
+    eMailValid = true;
+  }
+}
